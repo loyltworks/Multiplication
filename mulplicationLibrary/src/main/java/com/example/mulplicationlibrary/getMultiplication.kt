3a3ed50aa44908d1825bf76a1d5c11a3cc1a76a8
Loyltw0rks <loyltwo3ks.developers@gmail.com>
2024-07-13 16:56:36 +0530

    intial commit

---
+++ mulplicationLibrary/src/main/java/com/example/mulplicationlibrary/getMultiplication.kt
@@ -0,0 +1,10 @@
+package com.example.mulplicationlibrary
+
+object getMultiplication {
+
+    fun getMultiplication(firstValue:Int,secondValue:Int):Int{
+
+
+        return firstValue * secondValue
+    }
+}
