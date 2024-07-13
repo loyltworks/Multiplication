08a19d053bfb8e9f4600478614db382645ba6a1a
Loyltw0rks <loyltwo3ks.developers@gmail.com>
2024-07-13 17:06:11 +0530

    intial commit

--- app/src/main/java/com/example/multiplicationlibrarytask/MainActivity.kt
+++ app/src/main/java/com/example/multiplicationlibrarytask/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.multiplicationlibrarytask
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.mulplicationlibrary.getMultiplication
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        Log.e("IDIDIDDI","IDID "+getMultiplication.getMultiplication(3,5))
     }
 }
